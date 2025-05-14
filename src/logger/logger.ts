3b3a9db027af772627bdb344ba94d5630c94550c
HuanCanhCut <tronghuanxxx@gmail.com>
2025-05-14 14:41:59 +0700

    refactor: improve logger functionality by creating a log directory and updating log file path

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,14 +1,22 @@
 import winston from 'winston'
 import moment from 'moment-timezone'
+import fs from 'fs'
+import path from 'path'
 
 const { combine, timestamp, printf } = winston.format
+
+const logDir = 'log'
+const logFile = path.join(logDir, 'error.log')
+
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir)
+}
 
 const customFormat = printf(({ level, message, timestamp }) => {
     const vietnamTime = moment(timestamp as string)
         .tz('Asia/Ho_Chi_Minh')
         .format('YYYY-MM-DD HH:mm:ss.SSS')
-    return `
-==============================================================================
+    return ` ==============================================================================
 ${vietnamTime} 
 [${level}]: ${message}
     `
@@ -17,7 +25,7 @@ ${vietnamTime}
 const logger = winston.createLogger({
     level: 'error',
     format: combine(timestamp(), customFormat),
-    transports: [new winston.transports.File({ filename: 'error.log', level: 'error' })],
+    transports: [new winston.transports.File({ filename: logFile, level: 'error' })],
 })
 
 export default logger
