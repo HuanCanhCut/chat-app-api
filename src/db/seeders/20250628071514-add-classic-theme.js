9aa31f43f3e965ab91d3ae5ad7f5021ceda8c9da
HuanCanhCut <tronghuanxxx@gmail.com>
2025-07-11 10:46:50 +0700

    refactor: update theme seeder for 'Cổ điển' and 'class_of_25' themes, ensuring proper structure and adding ignoreDuplicates option

--- src/db/seeders/20250628071514-add-classic-theme.js
+++ src/db/seeders/20250628071514-add-classic-theme.js
@@ -12,56 +12,62 @@ module.exports = {
          *   isBetaMember: false
          * }], {});
          */
-        await queryInterface.bulkInsert('conversation_themes', [
+        await queryInterface.bulkInsert(
+            'conversation_themes',
+            [
+                {
+                    name: 'Cổ điển',
+                    logo: 'https://res.cloudinary.com/dkmwrkngj/image/upload/v1751185960/images-removebg-preview_t36s8e.png',
+                    description: null,
+                    theme_config: JSON.stringify({
+                        sender: {
+                            light: {
+                                text_color: '#fff',
+                                background_color: '#0099ff',
+                            },
+                            dark: {
+                                text_color: '#fff',
+                                background_color: '#0E92EB',
+                            },
+                        },
+                        receiver: {
+                            light: {
+                                text_color: '#000',
+                                background_color: '#F0F0F0',
+                            },
+                            dark: {
+                                text_color: '#fff',
+                                background_color: '#303030',
+                            },
+                        },
+                        background_theme: {
+                            light: {
+                                background: '#fff',
+                                header_color: '#ffffff',
+                                footer_color: '#ffffff',
+                            },
+                            dark: {
+                                background: '#212223',
+                                header_color: '#212223',
+                                footer_color: '#212223',
+                            },
+                        },
+                        reply_message: {
+                            light: {
+                                background_color: '#F6F6F6',
+                            },
+                            dark: {
+                                background_color: '#8383838d',
+                            },
+                        },
+                    }),
+                    emoji: '1f44d',
+                },
+            ],
             {
-                name: 'Cổ điển',
-                logo: 'https://res.cloudinary.com/dkmwrkngj/image/upload/v1751185960/images-removebg-preview_t36s8e.png',
-                description: null,
-                theme_config: JSON.stringify({
-                    sender: {
-                        light: {
-                            text_color: '#fff',
-                            background_color: '#0099ff',
-                        },
-                        dark: {
-                            text_color: '#fff',
-                            background_color: '#0E92EB',
-                        },
-                    },
-                    receiver: {
-                        light: {
-                            text_color: '#000',
-                            background_color: '#F0F0F0',
-                        },
-                        dark: {
-                            text_color: '#fff',
-                            background_color: '#303030',
-                        },
-                    },
-                    background_theme: {
-                        light: {
-                            background: '#fff',
-                            header_color: '#ffffff',
-                            footer_color: '#ffffff',
-                        },
-                        dark: {
-                            background: '#212223',
-                            header_color: '#212223',
-                            footer_color: '#212223',
-                        },
-                    },
-                    reply_message: {
-                        light: {
-                            background_color: '#F6F6F6',
-                        },
-                        dark: {
-                            background_color: '#8383838d',
-                        },
-                    },
-                }),
-                emoji: '1f44d',
+                ignoreDuplicates: true,
             },
-        ])
+        )
     },
 
     async down(queryInterface, Sequelize) {
